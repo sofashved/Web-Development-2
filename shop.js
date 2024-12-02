c221949d0fbe58db0f0aeee53430814beb3fc09e
sofashved <sofy.sergeevna06@gmail.com>
2024-12-02 21:16:26 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -28,3 +28,65 @@ async function getResponse() {
 
 }
 getResponse()
+
+
+
+function search(){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce1()}
+}
+
+async function getResponce1() {
+
+    let responce = await fetch("data.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 11)
+    console.log(content)
+    let key
+
+    content_title=content.sort((a, b) => {
+    const nameA = a.title.toUpperCase(); 
+    const nameB = b.title.toUpperCase(); 
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      // names must be equal
+      return 0;
+    });
+
+
+    content_filter=[]
+    let word=document.getElementById('search').value.toLowerCase();
+    content_filter= content_title.filter((product) =>{
+        return (
+                    product.title.toLowerCase().includes(word) ||
+                    product.description.toLowerCase().includes(word) ||
+                    product.price.toString().includes(word)
+                );
+
+    });
+    console.log(content_filter);
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    for (key in content_filter) {
+                node_for_insert.innerHTML += `
+                <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
+                <h5 class="card-title">${content_filter[key].title}</h5>
+                <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
+                <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
+                <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
+                </li>
+                        `
+            }
+
+}
+
+sort()
