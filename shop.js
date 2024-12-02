50c90bb57a24130262c92af88241a03a0b5aa068
sofashved <sofy.sergeevna06@gmail.com>
2024-12-02 21:19:46 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,42 +1,34 @@
-async function getResponse() {
-    let response = await fetch("data.json")
-    let content = await response.text()
-    console.log(content)
-    content = JSON.parse(content)
-    content = content.splice(0, 9)
-    console.log(content)
 
-    let key
-    for (key in content) {
-        console.log(content[key].id, content[key].title)
-        console.log(content[key])
-    }
+function sort(){
+    let price=document.getElementById("price")
+    let title=document.getElementById("title")
+    if(price.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce()}
 
-    let node_for_insert = document.getElementById("node_for_insert")
-    for (key in content) {
-        node_for_insert.innerHTML += `
-        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
-        <img style="width: 180px" class="align-self-center" src=${content[key].img}>
-        <h5 class="card-title">${content[key].title}</h5>
-        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
-        </li>
-                `
-    }
-
-
-}
-getResponse()
-
-
-
-function search(){
+    if(title.checked){
         document.getElementById('node_for_insert').innerHTML = '';
         getResponce1()}
 }
 
-async function getResponce1() {
+function search(){
+
+
+
+
+
+    if(price.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce()}
+
+    if(title.checked){
+        document.getElementById('node_for_insert').innerHTML = '';
+        getResponce1()}
+}
+
+
+
+async function getResponce() {
 
     let responce = await fetch("data.json")
 
@@ -44,12 +36,78 @@ async function getResponce1() {
     console.log(content)
     content = JSON.parse(content)
     content = content.splice(0, 11)
+    //content.sort()
     console.log(content)
     let key
+    /*for (key in content) {
+        console.log(content[key].id, content[key].title)
+        console.log(content[key])
+    }*/
+    content_price=content.sort((a, b) => a.price - b.price);
 
+    /*
+     products: []
+     filteredProducts: []
+     const query = this.searchQuery.toLowerCase();
+            this.filteredProducts = this.products.filter(product => {
+                return (
+                    product.title.toLowerCase().includes(query) ||
+                    product.description.toLowerCase().includes(query) ||
+                    product.price.toString().includes(query)
+                );
+            });
+    */
+   content_filter=[]
+    let word=document.getElementById('search').value.toLowerCase();
+   //let word = 'search'.toLowerCase();
+    content_filter= content_price.filter((product) =>{
+        return (
+                    product.title.toLowerCase().includes(word) ||
+                    product.description.toLowerCase().includes(word) ||
+                    product.price.toString().includes(word)
+                );
+
+    });
+    console.log(content_filter);
+
+    let node_for_insert = document.getElementById("node_for_insert")
+    //node_for_insert.innerHTML='';
+    for (key in content_filter) {
+                node_for_insert.innerHTML += `
+                <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
+                <h5 class="card-title">${content_filter[key].title}</h5>
+                <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
+                <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
+                <p class="card-text" >!!Заказать!! <input class="w-25" type="text" value="0" name="check"></p>
+                </li>
+                        `
+            }
+
+}
+async function getResponce1() {
+   // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
+    //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
+    //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
+//    let responce = await fetch("shop.json")
+    let responce = await fetch("shop.json")
+
+    let content = await responce.text()
+    console.log(content)
+    content = JSON.parse(content)
+    content = content.splice(0, 11)
+    //content.sort()
+    console.log(content)
+    let key
+    /*for (key in content) {
+        console.log(content[key].id, content[key].title)
+        console.log(content[key])
+    }*/
+
+    // sort by name
     content_title=content.sort((a, b) => {
-    const nameA = a.title.toUpperCase(); 
-    const nameB = b.title.toUpperCase(); 
+    const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.title.toUpperCase(); // ignore upper and lowercase
       if (nameA < nameB) {
         return -1;
       }
@@ -64,6 +122,7 @@ async function getResponce1() {
 
     content_filter=[]
     let word=document.getElementById('search').value.toLowerCase();
+   //let word = 'search'.toLowerCase();
     content_filter= content_title.filter((product) =>{
         return (
                     product.title.toLowerCase().includes(word) ||
@@ -74,6 +133,7 @@ async function getResponce1() {
     });
     console.log(content_filter);
 
+    //node_for_insert.innerHTML='';
     let node_for_insert = document.getElementById("node_for_insert")
     for (key in content_filter) {
                 node_for_insert.innerHTML += `
