aa6a0d3426c64b75d2da3147ac6208356a26ba7b
sofashved <sofy.sergeevna06@gmail.com>
2024-12-03 19:31:29 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -3,7 +3,7 @@ async function getResponse() {
     let content = await response.text()
     console.log(content)
     content = JSON.parse(content)
-    content = content.splice(0, 25)
+    content = content.splice(0, 12)
     console.log(content)
 
     let key
